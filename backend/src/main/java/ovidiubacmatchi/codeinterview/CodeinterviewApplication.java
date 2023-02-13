cbfec209d530555d95f58d27938433c800f57fbd
Ovidiu Bachmatchi <ovidiubachmatchi@gmail.com>
2023-02-13 16:30:38 +0200

    backend init

---
+++ backend/src/main/java/ovidiubacmatchi/codeinterview/CodeinterviewApplication.java
@@ -0,0 +1,13 @@
+package ovidiubacmatchi.codeinterview;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CodeinterviewApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CodeinterviewApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/ovidiubacmatchi/codeinterview/CodeinterviewApplicationTests.java
@@ -0,0 +1,13 @@
+package ovidiubacmatchi.codeinterview;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CodeinterviewApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
